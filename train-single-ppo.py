b87534e1897e74ff69f7fd3f292c7dafcf08f6c7
sandrololo <sandro2000h@gmail.com>
2025-10-17 11:06:24 +0200

    add more training parameters

--- train-single-ppo.py
+++ train-single-ppo.py
@@ -3,10 +3,10 @@ from ray import tune
 import ray
 from ray.rllib.algorithms.ppo import PPOConfig
 from ray.rllib.core.rl_module.default_model_config import DefaultModelConfig
-from ray.air.integrations.wandb import WandbLoggerCallback, setup_wandb
+from ray.air.integrations.wandb import WandbLoggerCallback
 
 from car_racing_env import CarRacingEnv
-
+from wandbvideocallback import WandbVideoCallback
 
 # Configure the algorithm.
 config = (
@@ -22,26 +22,30 @@ config = (
         ),
     )
     .env_runners(
-        num_env_runners=6, sample_timeout_s=1500
+        num_env_runners=6, num_envs_per_env_runner=4, sample_timeout_s=1500
     )  # makes sense to have as many runners and therefore as much data as possible
     .learners(num_learners=1, num_gpus_per_learner=1)
     # only 1 runner and low interval for evaluation as we have new data every iteration anyways
     .training(
+        gamma=0.95,
         use_critic=True,
         use_gae=True,
-        train_batch_size=128, 
-        lr=0.00001,
-        grad_clip=0.3,
+        train_batch_size=512,
+        shuffle_batch_per_epoch=True,
+        lr=0.0000001,
+        grad_clip=0.1,
+        kl_coeff=0.1,
         grad_clip_by="norm",
-        num_epochs=10,
+        num_epochs=5,
     )
     .evaluation(
         evaluation_interval=5,
         evaluation_num_env_runners=1,
-        evaluation_sample_timeout_s=1500,
-        evaluation_duration=3,
+        evaluation_sample_timeout_s=3000,
+        evaluation_duration=5,
         evaluation_duration_unit="episodes",
     )
+    .callbacks(WandbVideoCallback)
 )
 
 ray.init()
@@ -54,15 +58,16 @@ results = tune.Tuner(
     ),
     param_space=config,
     run_config=tune.RunConfig(
-        stop={"training_iteration": 200},
+        stop={"training_iteration": 100},
         verbose=1,
         storage_path=os.path.join(os.getcwd(), "results/single-agent"),
         callbacks=[
             WandbLoggerCallback(
+                group="car-racing",
                 project="car-racing-single-agent",
                 log_config=True,
-                upload_checkpoints=True
+                upload_checkpoints=True,
             )
-        ]
+        ],
     ),
 ).fit()
