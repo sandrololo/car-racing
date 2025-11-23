47b70f4bdc3bb73a70b7e998c1004c8d5b2c0843
sandrololo <sandro2000h@gmail.com>
2025-11-23 21:41:25 +0100

    copy implementation of step method to car

--- environments/multiagent/cars.py
+++ environments/multiagent/cars.py
@@ -1,7 +1,11 @@
 import math
 from typing import Union
 import numpy as np
-from gymnasium.envs.box2d.car_dynamics import Car
+from gymnasium.envs.box2d.car_dynamics import (
+    Car,
+    SIZE as CAR_SIZE,
+    WHEEL_MOMENT_OF_INERTIA,
+)
 from gymnasium.error import DependencyNotInstalled
 
 try:
@@ -31,6 +35,8 @@ class _CarInfo:
         self.count = _CarInfo.car_count
         self.id = f"car_{self.count}"
         _CarInfo.car_count += 1
+        self.friction_limit = 1000000 * CAR_SIZE * CAR_SIZE
+        self.engine_power = 100000000 * CAR_SIZE * CAR_SIZE
 
     @property
     def position(self) -> tuple[float, float]:
@@ -104,7 +110,96 @@ class _CarInfo:
         self.car.brake(action[2])
 
     def step(self, dt: float):
-        self.car.step(dt)
+        for w in self.wheels:
+            # Steer each wheel
+            dir = np.sign(w.steer - w.joint.angle)
+            val = abs(w.steer - w.joint.angle)
+            w.joint.motorSpeed = dir * min(50.0 * val, 3.0)
+
+            # Position => friction_limit
+            grass = True
+            friction_limit = self.friction_limit * 0.6  # Grass friction if no tile
+            for tile in w.tiles:
+                friction_limit = max(
+                    friction_limit, self.friction_limit * tile.road_friction
+                )
+                grass = False
+
+            # Force
+            forw = w.GetWorldVector((0, 1))
+            side = w.GetWorldVector((1, 0))
+            v = w.linearVelocity
+            vf = forw[0] * v[0] + forw[1] * v[1]  # forward speed
+            vs = side[0] * v[0] + side[1] * v[1]  # side speed
+
+            # add small coef not to divide by zero
+            w.omega += (
+                dt
+                * self.engine_power
+                * w.gas
+                / WHEEL_MOMENT_OF_INERTIA
+                / (abs(w.omega) + 5.0)
+            )
+            self.fuel_spent += dt * self.engine_power * w.gas
+
+            if w.brake >= 0.9:
+                w.omega = 0
+            elif w.brake > 0:
+                BRAKE_FORCE = 15  # radians per second
+                dir = -np.sign(w.omega)
+                val = BRAKE_FORCE * w.brake
+                if abs(val) > abs(w.omega):
+                    val = abs(w.omega)  # low speed => same as = 0
+                w.omega += dir * val
+            w.phase += w.omega * dt
+
+            vr = w.omega * w.wheel_rad  # rotating wheel speed
+            f_force = -vf + vr  # force direction is direction of speed difference
+            p_force = -vs
+
+            # Physically correct is to always apply friction_limit until speed is equal.
+            # But dt is finite, that will lead to oscillations if difference is already near zero.
+
+            # Random coefficient to cut oscillations in few steps (have no effect on friction_limit)
+            f_force *= 205000 * CAR_SIZE * CAR_SIZE
+            p_force *= 205000 * CAR_SIZE * CAR_SIZE
+            force = np.sqrt(np.square(f_force) + np.square(p_force))
+
+            # Skid trace
+            if abs(force) > 2.0 * friction_limit:
+                if (
+                    w.skid_particle
+                    and w.skid_particle.grass == grass
+                    and len(w.skid_particle.poly) < 30
+                ):
+                    w.skid_particle.poly.append((w.position[0], w.position[1]))
+                elif w.skid_start is None:
+                    w.skid_start = w.position
+                else:
+                    w.skid_particle = self.car._create_particle(
+                        w.skid_start, w.position, grass
+                    )
+                    w.skid_start = None
+            else:
+                w.skid_start = None
+                w.skid_particle = None
+
+            if abs(force) > friction_limit:
+                f_force /= force
+                p_force /= force
+                force = friction_limit  # Correct physics here
+                f_force *= force
+                p_force *= force
+
+            w.omega -= dt * f_force * w.wheel_rad / WHEEL_MOMENT_OF_INERTIA
+
+            w.ApplyForceToCenter(
+                (
+                    p_force * side[0] + f_force * forw[0],
+                    p_force * side[1] + f_force * forw[1],
+                ),
+                True,
+            )
 
     def new_lap(self):
         self.lap_count += 1
