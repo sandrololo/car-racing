839659ad741003625019104cb35e859fa4edf731
@sandro.hilpert <sandro.hilpert@stud.hslu.ch>
2025-12-15 22:31:47 +0100

    add 5th car to curriculum

--- multi_agent_config.py
+++ multi_agent_config.py
@@ -5,7 +5,7 @@ FIRST_TILE_VISITOR_REWARD_FACTOR = 1
 OBS_FRAME_STACK = 4
 NORMALIZE_REWARDS = True
 NUM_ENV_RUNNERS = 6
-HEAD_FCNET_HIDDENS = [64, 64]
+HEAD_FCNET_HIDDENS = [128, 64]
 ROLLOUT_FRAGMENT_LENGTH = 800
 TRAIN_MAX_TIMESTEPS_START = 500
 TRAIN_MAX_TIMESTEPS_PER_EPISODE_INCREASE = 0.1
@@ -14,9 +14,9 @@ TRAIN_NUM_ITERATIONS = 5000
 MINI_BATCH_SIZE = 256
 TRAIN_CLIP_PARAM = 0.1
 TRAIN_NUM_EPOCHS = 5
-LR_SCHEDULE_START = 0.0001
-LR_SCHEDULE_END = 0.0001
-EVAL_INTERVAL = 50
+LR_SCHEDULE_START = 0.00003
+LR_SCHEDULE_END = 0.00003
+EVAL_INTERVAL = 75
 EVAL_MAX_TIMESTEPS = 800
 EVAL_DURATION = 3
 NUM_CARS = 1
@@ -35,7 +35,8 @@ CURRICULUM_CONFIG = CurriculumConfig(
     num_cars_start=1,
     entries=[
         CurriculumStep(num_cars=2, min_reward=600),
-        CurriculumStep(num_cars=3, min_reward=800),
-        CurriculumStep(num_cars=4, min_reward=1000),
+        CurriculumStep(num_cars=3, min_reward=900),
+        CurriculumStep(num_cars=4, min_reward=1200),
+        CurriculumStep(num_cars=5, min_reward=1500),
     ],
 )
